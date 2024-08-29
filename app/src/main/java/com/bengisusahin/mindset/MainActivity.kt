7ead5ad80c53a95a16a300d2e4556c7b090fd5dd
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-08-29 16:12:06 +0300

    feat: added switch control functionality

--- app/src/main/java/com/bengisusahin/mindset/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/mindset/MainActivity.kt
@@ -5,16 +5,50 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bengisusahin.mindset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.switchEgo.isChecked = true
+        updateSwitchesState()
+
+        binding.switchEgo.setOnCheckedChangeListener { _, isChecked ->
+            updateSwitchesState()
+        }
     }
+
+    private fun updateSwitchesState() {
+        val isEgoChecked = binding.switchEgo.isChecked
+
+        binding.apply {
+            if (isEgoChecked) {
+                switchHappiness.isChecked = false
+                switchOptimism.isChecked = false
+                switchKindness.isChecked = false
+                switchGiving.isChecked = false
+                switchRespect.isChecked = false
+            }
+
+            switchHappiness.isEnabled = !isEgoChecked
+            switchOptimism.isEnabled = !isEgoChecked
+            switchKindness.isEnabled = !isEgoChecked
+            switchGiving.isEnabled = !isEgoChecked
+            switchRespect.isEnabled = !isEgoChecked
+        }
+    }
+
 }

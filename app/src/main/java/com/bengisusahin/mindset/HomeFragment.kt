24fd8aa8fb89bd1b1159b8925e3caed5d9846442
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-08-30 00:16:52 +0300

    feat: added dynamic bottom navigation bar according to switch states

--- app/src/main/java/com/bengisusahin/mindset/HomeFragment.kt
+++ app/src/main/java/com/bengisusahin/mindset/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.bengisusahin.mindset
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import com.bengisusahin.mindset.databinding.FragmentHomeBinding
@@ -28,10 +29,13 @@ class HomeFragment : Fragment() {
         binding.switchEgo.isChecked = true
         updateSwitchesState()
 
+        (activity as MainActivity).binding.bottomNavigation.visibility =
+            if (binding.switchEgo.isChecked) View.GONE else View.VISIBLE
+
         binding.switchEgo.setOnCheckedChangeListener { _, isChecked ->
             updateSwitchesState()
             (activity as MainActivity).binding.bottomNavigation.visibility =
-                if (isChecked) View.VISIBLE else View.GONE
+                if (isChecked) View.GONE else View.VISIBLE
         }
     }
 
@@ -39,21 +43,36 @@ class HomeFragment : Fragment() {
         val isEgoChecked = binding.switchEgo.isChecked
 
         binding.apply {
-            val switches = listOf(switchHappiness, switchOptimism, switchKindness, switchGiving, switchRespect)
+            val switches = listOf(
+                Triple(switchHappiness, R.drawable.ic_happiness, getString(R.string.happiness)),
+                Triple(switchOptimism, R.drawable.ic_optimism, getString(R.string.optimism)),
+                Triple(switchKindness, R.drawable.ic_kindness, getString(R.string.kindness)),
+                Triple(switchGiving, R.drawable.ic_giving, getString(R.string.giving)),
+                Triple(switchRespect, R.drawable.ic_respect, getString(R.string.respect))
+            )
 
             // Enable/disable switches based on Ego switch state
-            switches.forEach { switch ->
+            switches.forEach { (switch, iconResId, title) ->
+                switch.setOnCheckedChangeListener(null)
+
                 switch.isChecked = false
                 switch.isEnabled = !isEgoChecked
+
+                switch.setOnCheckedChangeListener { _, isChecked ->
+                    val bottomNavigationView = (activity as MainActivity).binding.bottomNavigation
+                    if (isChecked) {
+                        bottomNavigationView.menu.add(Menu.NONE, switch.id, Menu.NONE, title)
+                            .setIcon(iconResId)
+                    } else {
+                        bottomNavigationView.menu.removeItem(switch.id)
+                    }
+                }
             }
         }
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
